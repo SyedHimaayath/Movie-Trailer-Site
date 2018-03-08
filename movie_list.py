08e41cb89288963d28c5ea0cc06479101f03bc6c
Syed Himaayath <syedhimaayath155@gmail.com>
2018-03-08 13:15:52 +0530

    added functionality

--- movie_list.py
+++ movie_list.py
@@ -1,19 +1,22 @@
 import fresh_tomatoes
 import parents
 
-toystory = media.Movie("Toy Story","Bla bla bla","https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0%2C0%2C300%2C450","https://www.youtube.com/watch?v=SgoiKLFBA3Q")
+toystory = parents.Movie("Toy Story","Bla bla bla","https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0%2C0%2C300%2C450","https://www.youtube.com/watch?v=SgoiKLFBA3Q")
 
-inferno = media.Movie("Inferno","Bla bla bla","http://www.sonypictures.com/movies/inferno/assets/images/onesheet.jpg","https://www.youtube.com/watch?v=RH2BD49sEZI")
+inferno = parents.Movie("Inferno","Bla bla bla","http://www.sonypictures.com/movies/inferno/assets/images/onesheet.jpg","https://www.youtube.com/watch?v=RH2BD49sEZI")
 
-school_of_rock = media.Movie("School Of Rock","Bla bla bla","https://images-na.ssl-images-amazon.com/images/I/51XB8K6WPEL._SY445_.jpg","https://www.youtube.com/watch?v=3PsUJFEBC74")
+school_of_rock = parents.Movie("School Of Rock","Bla bla bla","https://images-na.ssl-images-amazon.com/images/I/51XB8K6WPEL._SY445_.jpg","https://www.youtube.com/watch?v=3PsUJFEBC74")
 
-hungergames = media.Movie("Hunger Games","Bla bla bla","https://images-na.ssl-images-amazon.com/images/I/41LPBRNaVCL._SX355_BO1,204,203,200_.jpg","https://www.youtube.com/watch?v=mfmrPu43DF8")
+hungergames = parents.Movie("Hunger Games","Bla bla bla","https://images-na.ssl-images-amazon.com/images/I/41LPBRNaVCL._SX355_BO1,204,203,200_.jpg","https://www.youtube.com/watch?v=mfmrPu43DF8")
 
-angels_and_demons = media.Movie("Angels and Demons","Bla bla bla","https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Angels_and_demons.jpg/220px-Angels_and_demons.jpg","https://www.youtube.com/watch?v=dhMQVeL8Kqw")
+angels_and_demons = parents.Movie("Angels and Demons","Bla bla bla","https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Angels_and_demons.jpg/220px-Angels_and_demons.jpg","https://www.youtube.com/watch?v=dhMQVeL8Kqw")
 
-titanic = media.Movie("Titanic","Bla bla bla","https://images-na.ssl-images-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg","https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+titanic = parents.Movie("Titanic","Bla bla bla","https://images-na.ssl-images-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg","https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
 
-red_sparrow = media.Movie("Red Sparrow","Bla bla bla","https://assets.voxcinemas.com/posters/P_HO00005162.jpg","https://www.youtube.com/watch?v=PmUL6wMpMWw")
+red_sparrow = parents.Movie("Red Sparrow","Bla bla bla","https://assets.voxcinemas.com/posters/P_HO00005162.jpg","https://www.youtube.com/watch?v=PmUL6wMpMWw")
 
-death_wish = media.Movie("Death Wish","Bla bla bla","https://www.dvdsreleasedates.com/posters/800/D/Death-Wish-2017-movie-poster.jpg","https://www.youtube.com/watch?v=HzILu6yyA20")
+death_wish = parents.Movie("Death Wish","Bla bla bla","https://www.dvdsreleasedates.com/posters/800/D/Death-Wish-2017-movie-poster.jpg","https://www.youtube.com/watch?v=HzILu6yyA20")
 
+movies = [toystory, inferno, school_of_rock, hungergames, angels_and_demons, titanic, red_sparrow, death_wish]
+
+fresh_tomatoes.open_movies_page(movies)
--- parents.py
+++ parents.py
@@ -1,12 +1,8 @@
-class Video():
-    def __init__(self, title, duration):
-        self.title = title
-        self.duration = duration
 
-class Movie(Video):
-    def __init__(self, title, duration, movie_storyline, movie_poster, movie_youtube_url):
-        Video.__init__(title, duration)
+class Movie():
+    def __init__(self, title, movie_storyline, movie_poster, trailer_youtube_url):
+        self.title = title
         self.storyline = movie_storyline
-        self.poster = movie_poster
-        self.youtube_url = movie_youtube_url
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = trailer_youtube_url
 
